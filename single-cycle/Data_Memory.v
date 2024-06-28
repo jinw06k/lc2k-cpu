10781c9ae253d015baca1260b20ce7eb3c289866
jinw06k <jinuk1024@gmail.com>
2024-06-28 10:24:15 -0700

    new vcd

--- single-cycle/Data_Memory.v
+++ single-cycle/Data_Memory.v
@@ -17,13 +17,13 @@ module Data_Memory(
         end
 
         // subone
-        // Data[12] = -1;
-        // Data[13] = 2;
-        // Data[14] = 5;
-        // Data[15] = 7;
+        Data[12] = -1;
+        Data[13] = 2;
+        Data[14] = 5;
+        Data[15] = 7;
 
         // jalrTest
-        Data[7] = 5;
+        // Data[7] = 5;
 
     end
 
--- single-cycle/Instr_Memory.v
+++ single-cycle/Instr_Memory.v
@@ -9,27 +9,27 @@ module Instr_Memory(
     initial begin
 
         // subone
-        // Instruction[0] = 32'd8454158;
-        // Instruction[1] = 32'd8519693;
-        // Instruction[2] = 32'd8585228;
-        // Instruction[3] = 32'd17432578;
-        // Instruction[4] = 32'd720897;
-        // Instruction[5] = 32'd16842749;
-        // Instruction[6] = 32'd29360128;
-        // Instruction[7] = 32'd65540;
-        // Instruction[8] = 32'd8454159;
-        // Instruction[9] = 32'd12845071;
-        // Instruction[10] = 32'd8454159;
-        // Instruction[11] = 32'd25165824;
+        Instruction[0] = 32'd8454158;
+        Instruction[1] = 32'd8519693;
+        Instruction[2] = 32'd8585228;
+        Instruction[3] = 32'd17432578;
+        Instruction[4] = 32'd720897;
+        Instruction[5] = 32'd16842749;
+        Instruction[6] = 32'd29360128;
+        Instruction[7] = 32'd65540;
+        Instruction[8] = 32'd8454159;
+        Instruction[9] = 32'd12845071;
+        Instruction[10] = 32'd8454159;
+        Instruction[11] = 32'd25165824;
 
         // jalrTest
-        Instruction[0] = 32'd8519687;
-        Instruction[1] = 32'd22216704;
-        Instruction[2] = 32'd16777219;
-        Instruction[3] = 32'd29360128;
-        Instruction[4] = 32'd29360128;
-        Instruction[5] = 32'd1179649;
-        Instruction[6] = 32'd25165824;
+        // Instruction[0] = 32'd8519687;
+        // Instruction[1] = 32'd22216704;
+        // Instruction[2] = 32'd16777219;
+        // Instruction[3] = 32'd29360128;
+        // Instruction[4] = 32'd29360128;
+        // Instruction[5] = 32'd1179649;
+        // Instruction[6] = 32'd25165824;
     end
 
     assign instr = Instruction[pcCurrent];
